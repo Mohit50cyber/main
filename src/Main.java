72cade4f260c212f8fbb36dccce954e886eee4a4
Mohit50cyber <68845963+Mohit50cyber@users.noreply.github.com>
2022-01-06 03:05:25 +0530

    commited

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("debug");
+        int myarr[]={2,5,7,81};
+        int sum = getSum(myarr);
+        System.out.println("sum = " + sum);
+    }
+
+    public static int getSum(int arr[]){
+        int s=0;
+        for(int i = 0;i<arr.length;i++){
+            s=s+arr[i];
+        }
+        s= s+3;
+        return s;
+    }
+}
